0551b215a81b2dfa6a6913ac6b3ad0a9552cd3f7
xjsongphy <m18892286119@163.com>
2024-03-07 11:05:30 +0800

    update

--- update.bat
+++ update.bat
@@ -3,6 +3,6 @@ git config --global --unset https.proxy
 git add .
 git commit -m "update"
 git branch -M main
-git remote add origin https://github.com/xjsongphy/2024spring-cs201.git
+git remote add origin https://github.com/xjsongphy/2023fall-cs101.git
 git push -u origin main
 pause
